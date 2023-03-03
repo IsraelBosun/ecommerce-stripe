cd22eb3b726acdb53c9ccc80d4b5af1450ae2fae
IsraelBosun <israelbosun1@gmail.com>
2023-03-03 18:30:31 +0100

    finished main page and products page

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { Product, FooterBanner, HeroBanner } from "../components/index.js"
 const Home = ({  products, bannerData }) => {
   return (
     <>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+      <HeroBanner heroBanner={bannerData[0]}/>
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
